715d31daf193f9781aed477fd2c255399f87804e
Anna <anna.zierlinger@stud.fh-campuswien.ac.at>
2025-06-02 00:04:48 +0200

    user with username

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },   // NEU
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
+
 
 module.exports = mongoose.model('User', userSchema);
