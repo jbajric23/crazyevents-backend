c22f5978e1758c1c371372ae05d3d721225ba15e
Jasmin Bajric <jasmin.bajric@stud.fh-campuswien.ac.at>
2025-06-02 00:23:20 +0200

    feat: GoingTo implemented in BE.

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -12,23 +12,19 @@ module.exports = (server) => {
     });
 
     server.get('/events', (req, res) => {
-        EventController.getEvents(req, res).then(r => {
-        });
+       EventController.getEvents(req, res).then(r => {});
     });
 
     server.get('/event/:title', (req, res) => {
-        EventController.getDummyEventsByTitle(req, res).then(r => {
-        });
+        EventController.getDummyEventsByTitle(req, res).then(r => {});
     })
 
     server.get('/events/:id', (req, res) => {
-        EventController.getSingleEventById(req, res).then(r => {
-        });
+        EventController.getSingleEventById(req, res).then(r => {});
     });
 
     server.post('/event', (req, res) => {
-        EventController.createDummyEvent(req, res).then(r => {
-        });
+        EventController.createDummyEvent(req, res).then(r => {});
     });
 
     /*****************************************************************
@@ -36,44 +32,35 @@ module.exports = (server) => {
      *****************************************************************/
 
     server.post('/register', async (req, res) => {
-        const { name, email, password } = req.body;
+        const { email, password } = req.body;
 
         try {
-            const exists = await User.findOne({email});
-            if (exists) return res.status(400).json({message: "User exists"});
+            const exists = await User.findOne({ email });
+            if (exists) return res.status(400).json({ message: "User exists" });
 
             const hash = await bcrypt.hash(password, 10);
-            const user = new User({name, email, password: hash});
+            const user = new User({ email, password: hash });
             await user.save();
 
-            res.status(201).json({message: "User created"});
+            res.status(201).json({ message: "User created" });
         } catch (err) {
-            res.status(500).json({message: "Server error"});
+            res.status(500).json({ message: "Server error" });
         }
     });
 
     server.post('/login', async (req, res) => {
-        const {email, password} = req.body;
+        const { email, password } = req.body;
 
         try {
-            const user = await User.findOne({email});
+            const user = await User.findOne({ email });
             if (!user || !(await bcrypt.compare(password, user.password))) {
-                return res.status(401).json({message: "Invalid credentials"});
+                return res.status(401).json({ message: "Invalid credentials" });
             }
 
-            const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: '1h'});
-
-            res.json({
-                token,
-                user: {
-                    id: user._id,
-                    email: user.email,
-                    name: user.name
-                }
-            });
+            const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+            res.json({ token });
         } catch (err) {
-            res.status(500).json({message: "Server error"});
+            res.status(500).json({ message: "Server error" });
         }
     });
-
 }
